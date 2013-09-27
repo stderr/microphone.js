2f41c1d5ce03646a63e130a3e47f2b1b6fa2219c
Josh Simpson <jsimpson@instructure.com>
2013-09-27 11:31:38 -0600

    Initial commit.

---
+++ main.js
@@ -0,0 +1,39 @@
+requirejs.config({
+  baseUrl: './lib',
+  paths: {
+    jquery: "../vendor/jquery-2.0.3.min"
+  },
+  shim: {
+    jquery: ["require"]
+  }
+});
+
+require(['jquery','microphone'], function($, microphone) {
+  var Microphone = microphone.create();
+
+  Microphone.add_command({
+    patterns: [new RegExp("display", "i")],
+    callback: function(mic, command) {
+      console.log(command.args[0]);
+      $("#" + command.args[0]).fadeIn();
+      mic.done();
+    }
+  });
+
+  Microphone.add_command({
+    patterns: [new RegExp("hide", "i")],
+    callback: function(mic, command) {
+      $("#" + command.args[0]).fadeOut();
+      mic.done();
+    }
+  });
+
+  $('#btn-start').click(function() {
+    Microphone.turn("on");
+  });
+
+  $('#btn-stop').click(function() {
+    Microphone.turn("off");
+  });
+});
+
