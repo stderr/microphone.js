44c7a2022df32db324d59f2771e439660b3e2578
Josh Simpson <jsimpson@instructure.com>
2013-09-27 17:21:51 -0600

    Add text-to-speech example.

--- main.js
+++ main.js
@@ -9,8 +9,11 @@ requirejs.config({
 });
 
 require(['jquery','microphone'], function($, microphone) {
-  var Microphone = microphone.create();
 
+  var Microphone = microphone.create();
+  // Examples:
+
+  // Control the DOM with your voice!
   Microphone.addCommand({
     patterns: [new RegExp("display", "i")],
     callback: function(mic, command) {
@@ -22,6 +25,25 @@ require(['jquery','microphone'], function($, microphone) {
     patterns: [new RegExp("hide", "i")],
     callback: function(mic, command) {
       $("#" + command.args[0]).fadeOut();
+    }
+  });
+
+  // Respond in kind!
+  Microphone.addCommand({
+    patterns: [new RegExp("hello", "i")],
+    callback: function(mic, command) {
+      var speaker = new Audio();
+      var message = "Hi, ";
+      $nameInput = $("#my-name");
+
+      if($nameInput.val()) {
+        message += $nameInput.val();
+      } else {
+        message += "stranger";
+      }
+
+      speaker.src = "http://tts-api.com/tts.mp3?q=" + escape(message);
+      speaker.play();
     }
   });
 
